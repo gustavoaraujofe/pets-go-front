569f34371d9c75c9a69de0137bbfdb0bf1dc0ec3
Gustavo Araujo <gustavoaraujofe@gmail.com>
2021-12-16 00:22:31 -0300

    fix

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apis = {
-  development: "http://localhost:4000/api/v1",
+  development: "http://localhost:4000/api/v1/",
   production: "https://pets-go-api.herokuapp.com/api/v1",
 };
 
