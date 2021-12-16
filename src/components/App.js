8b3460149d59cf5f495ef3bb03ae6ef03a4a6241
Gustavo Araujo <gustavoaraujofe@gmail.com>
2021-12-16 09:40:57 -0300

    test 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/home/Home";
+import Home from "../pages/Home/Home";
 import Signup from "../pages/auth/Signup";
 import Login from "../pages/auth/login/Login";
 
