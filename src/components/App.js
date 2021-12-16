1237e48b662283dc7b66e74a2d39b73a00ac4874
Gustavo Araujo <gustavoaraujofe@gmail.com>
2021-12-16 09:21:26 -0300

    teste css

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-
+import "../assets/styles/index.css"
 import Home from "../pages/home/Home";
 import Signup from "../pages/auth/Signup";
 import Login from "../pages/auth/login/Login";
